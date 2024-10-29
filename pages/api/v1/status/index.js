eef07c84d3831915cbc0b451e14c76ba8427e81d
Lais Golin <46529244+lgolin@users.noreply.github.com>
2024-10-29 21:45:03 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,36 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("select 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "esse é um teste" });
+  // Controller
+  const updatedAt = new Date().toISOString();
+
+  // max connections
+  const maxConnectionsResult = await database.query("show max_connections");
+  const maxConnections = parseInt(maxConnectionsResult.rows[0].max_connections);
+
+  // activeConnections
+  const databaseName = process.env.POSTGRES_DB;
+  const activeConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const activeConnections = activeConnectionsResult.rows[0].count;
+
+  // postgres version
+  const postgresVersionResult = await database.query("SHOW server_version");
+  const postgresVersion = String(postgresVersionResult.rows[0].server_version);
+
+  // View
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        postgres_version: postgresVersion,
+        max_connections: maxConnections,
+        active_connections: activeConnections,
+      },
+    },
+  });
 }
 
 export default status;
