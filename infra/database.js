8a4c4aed750c1e86de2dcab74080fe1f6a040138
Lais Golin <46529244+lgolin@users.noreply.github.com>
2024-11-03 17:21:18 +0000

    handles database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE.ENV === "development" ? false : true,
   });
   console.log("Credênciais do Postgres: ", {
     host: process.env.POSTGRES_HOST,
